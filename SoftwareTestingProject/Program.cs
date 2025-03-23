a3163828576d821ea6a2b2b57ac0385fad85d4e8
Gavin Meeker <gmeeke99@gmail.com>
2025-03-22 22:54:16 -0400

    add basic io functionality

--- SoftwareTestingProject/Program.cs
+++ SoftwareTestingProject/Program.cs
@@ -1,9 +1,47 @@
 ﻿namespace SoftwareTestingProject;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    public const string TestDirectoryName = "NewCoverageData";
+
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var testDataDirectoryPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..",
+            TestDirectoryName));
+
+        var testFiles = Directory.GetFiles(testDataDirectoryPath);
+        Array.Sort(testFiles);
+        foreach (var file in testFiles) ChangeTestResultTo(file, false);
+    }
+
+    private static void ChangeTestResultTo(string filePath, bool shouldPass = true)
+    {
+        var lines = File.ReadAllLines(filePath);
+        var firstLine = lines[0];
+        var firstPartOfLine = firstLine.Split(' ')[0];
+        lines[0] = firstPartOfLine += shouldPass ? " true" : " false";
+        File.WriteAllLines(filePath, lines);
+
+        RenameFile(filePath);
+    }
+
+    private static void RenameFile(string filePath)
+    {
+        var currentFileName = Path.GetFileName(filePath);
+
+        if (currentFileName.Contains("passed_") || currentFileName.Contains("failed_"))
+            currentFileName = Path.GetFileName(filePath).Split("_")[1];
+
+
+        using (var file = File.OpenText(filePath))
+        {
+            var parentDirectoryName = Path.GetFullPath(Path.Combine(filePath, ".."));
+            var testResultBool = Convert.ToBoolean(file.ReadLine()?.Split(" ")[1]);
+            var testResult = testResultBool ? "passed" : "failed";
+
+            var newFileName = Path.GetFullPath(Path.Combine(parentDirectoryName, $"{testResult}_{currentFileName}"));
+
+            File.Move(filePath, newFileName);
+        }
     }
 }
